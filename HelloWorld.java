d09eda54e0f074f3513e0f820e2d113ed39594ce
Ian T. Kelly <iankelly4165@gmail.com>
2018-09-13 22:30:27 -0400

    This commit includes changes to HelloWorld.java to insert a line of text, making a total of three lines, and to modify the last line of text.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
-        System.out.println("This is the end...of the program, that is.");
+        System.out.println("This is only the beginning.");
+        System.out.println("And this is the end...of the program, that is.");
     }
 }
